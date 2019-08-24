89a22d254d9035e59054bdce9720fdb381f8f6ce
bellis1120 <bellis1120@gmail.com>
2019-08-24 13:10:44 +0000

    next homework

---
+++ src/main/java/com/example/demo/ViewController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/")
+class ViewController {
+    @GetMapping
+    public String view() {
+        return "view";
+    }
+}
